f5d12638d22b11962170221083888bc601b2b687
PyAS-dev <yuanyuansg29@gmail.com>
2026-05-13 11:06:17 +0200

    Update algebra.py

--- algebra.py
+++ algebra.py
@@ -7,52 +7,40 @@ x, y = sp.symbols('x y')
 # Algebra Operations
 # -----------------------------
 
-def common_denominator(f, g):
+def CommonDenominator(f, g):
     f = sp.fraction(f)
     g = sp.fraction(g)
     return sp.lcm(f[1], g[1])
 
-def complete_square(P):
+def CompleteSquare(P):
     P = list(sp.Poly(P, x).all_coeffs())
     a = P[0]
     b = P[1]
     c = P[2]
     return sp.simplify(a*(x + b/(2*a))**2 + (c - b**2/(4*a)))
 
-def vector_cross_product(u, v):
+def CrossProduct(u, v):
     u = sp.Matrix(u)
     v = sp.Matrix(v)
     return u.cross(v)
 
-def complex_solutions(left, right, variable):
-    equation = sp.Eq(left, right)
-    return sp.solve(equation, variable)
-
-def complex_solutions_system(*equations, variables):
-    variables_ = list(variables)
-    sides = equations
-    if len(sides) % 2 != 0:
-        raise ValueError("You must pass left/right pairs")
-    equations = [sp.Eq(sides[i], sides[i + 1]) for i in range(0, len(sides), 2)]
-    return sp.solve(equations, variables)
-
-def division_integer(a, b):
+def DivisionInteger(a, b):
     return a // b
 
-def division_polynomial(P, Q):
+def DivisionPolynomial(P, Q):
     quotient, remainder = sp.div(P, Q)
     return quotient, remainder
 
-def divisors_count(n):
+def DivisorsCount(n):
     return sp.divisor_count(n)
 
-def divisors_list(n):
+def DivisorsList(n):
     return sp.divisors(n)
 
 def sigma(n):
     return sp.divisor_sigma(n)
 
-def vector_dot_product(u, v):
+def DotProduct(u, v):
     u = sp.Matrix(u)
     v = sp.Matrix(v)
     return u.dot(v)
@@ -60,25 +48,25 @@ def vector_dot_product(u, v):
 def expand(expression):
     return sp.expand(expression)
 
-def factorise(expression):
+def factor(expression):
     return sp.factor(expression)
 
-def from_base(n_string, base):
+def FromBase(n_string, base):
     return int(n_string, base)
 
 def gcd(*expressions):
     return sp.gcd(list(expressions))
 
-def irrational_factor(expression):
+def IrrationalFactor(expression):
     return "Did you really think factoring is simple? https://en.wikipedia.org/wiki/Field_extension"
 
-def is_factored(expression):
+def IsFactored(expression):
     if expression == factorise(expression):
         return True
     else:
         return False
 
-def is_prime(n):
+def IsPrime(n):
     return sp.isprime(n)
 
 def lcm(*expressions):
@@ -87,30 +75,30 @@ def lcm(*expressions):
 def maximum(*numbers):
     return sp.Max(numbers)
 
-def maximum_function(f, variable):
+def MaximumFunction(f, variable):
     return sp.maximum(f, variable)
 
 def minimum(*numbers):
     return sp.Min(numbers)
 
-def minimum_function(f, variable):
+def MinimumFunction(f, variable):
     return sp.minimum(f, variable)
 
-def mod_number(a, n):
+def ModNumber(a, n):
     return a % n
 
-def mod_polynomial(P, Q):
+def ModPolynomial(P, Q):
     return sp.rem(P, Q)
 
-def next_prime(n):
+def NextPrime(n):
     return sp.nextprime(n)
 
-def n_solve_equation(left, right, start, variable):
+def NSolveEquation(left, right, start, variable):
     equation = sp.Eq(left, right)
     if start is not None:
         return sp.nsolve(equation, variable, start)
     
-def n_solve_equation_system(*equations, variables):
+def NSolveEquationSystem(*equations, variables):
     variables_ = list(variables)
     sides = equations
     if len(sides) % 2 != 0:
@@ -118,31 +106,34 @@ def n_solve_equation_system(*equations, variables):
     equations = [sp.Eq(sides[i], sides[i + 1]) for i in range(0, len(sides), 2)]
     return sp.N(sp.solve(equations, variables))
 
-def previous_prime(n):
+def PreviousPrime(n):
     return sp.prevprime(n)
 
-def prime_factors(n):
+def PrimeFactors(n):
     return sp.factorint(n)
 
 def simplify(expression):
     return sp.simplify(expression)
 
-def solve_equation(left, right, variable):
+def SolveEquation(left, right, variable):
     equation = sp.Eq(left, right)
     return sp.solve(equation, variable)
 
-def solve_equation_system(*equations, variables):
-    variables_ = list(variables)
-    sides = equations
-    if len(sides) % 2 != 0:
-        raise ValueError("You must pass left/right pairs")
-    equations = [sp.Eq(sides[i], sides[i + 1]) for i in range(0, len(sides), 2)]
-    return sp.solve(equations, variables)
+def SolveEquationSystem(equations, variables):
+    if len(equations) % 2 != 0:
+        raise ValueError("Equations must be in left/right pairs (even number of items).")
+    
+    eqs = [
+        sp.Eq(equations[i], equations[i + 1])
+        for i in range(0, len(equations), 2)
+    ]
+    
+    return sp.solve(eqs, variables)
 
-def solve_inequality(expression):
+def SolveInequality(expression):
     return sp.solve_univariate_inequality(expression, x)
 
-def to_base(n, base):
+def ToBase(n, base):
     digits = []
     number = n
     while number > 0:
